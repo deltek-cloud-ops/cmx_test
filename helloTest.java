7dc2c7abaae885f96400aca78cf7ec563f78b7c7
leedeguzman1029 <45031851+leedeguzman1029@users.noreply.github.com>
2020-04-16 20:41:44 +0800

    Add files via upload

---
+++ helloTest.java
@@ -0,0 +1,9 @@
+package hello.world;
+
+import hello.world.datamodels.HelloWorldCardDataModel;
+
+import com.maconomy.api.container.launcher.MiContainerEvents;
+import com.maconomy.api.container.launcher.MiContainerFactory;
+import com.maconomy.toolkit.containers.McAbstractRootContainer;
+import com.maconomy.toolkit.containers.McContainerConfiguration;
+import com.maconomy.toolkit.containers.MiContainerConfiguration;
