c625473ad2b4435ee4d8ae8f8dbf313b8ecff69c
leedeguzman1029 <45031851+leedeguzman1029@users.noreply.github.com>
2020-04-22 23:24:33 +0800

    Delete testApi.java

--- cmx_txt_sub/sub/testApi.java
+++
@@ -1,6 +0,0 @@
-	@Override
-	protected MiContainerConfiguration.MiRoot defineConfiguration() {
-		final MiContainerConfiguration.MiRoot configuration = McContainerConfiguration.McRoot
-				.filterWithCard(HelloWorldCardDataModel.FACTORY);
-		return configuration;
-	}
