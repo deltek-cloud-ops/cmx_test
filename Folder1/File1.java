d78607e709c4dd3ef2f8422ee352577b3d667135
leedeguzman1029 <45031851+leedeguzman1029@users.noreply.github.com>
2020-04-23 20:40:45 +0800

    Add files via upload

--- Folder1/File1.java
+++ Folder1/File1.java
@@ -1,7 +1,41 @@
-StringBuilder query = new StringBuilder();
-query.append( "select * from user u where u.name in (" + namesString + ")" );
-try {
-	Connection connection = getConnection();
-    Statement statement = connection.createStatement();
-    resultSet = statement.executeQuery(query.toString());
+//
+//  ========================================================================
+//  Copyright (c) 1995-2013 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package com.acme;
+
+@SuppressWarnings("serial")
+public class Counter implements java.io.Serializable
+{
+    int counter=0;
+    String last;
+
+    public int getCount()
+    {
+	counter++;
+	return counter;
+    }
+
+    public void setLast(String uri) {
+        last=uri;
+    }
+
+    public String getLast() {
+        return last;
+    }
 }
+
